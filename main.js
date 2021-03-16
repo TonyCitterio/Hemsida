927b83640c316c52a4d86e670d78a0e36fe202bc
Tony Citterio <tonycitterio@MacBook-Pro-som-tillhor-Tony.local>
2021-03-16 13:44:04 +0100

    min forsta commit

---
+++ main.js
@@ -0,0 +1,11 @@
+document.getElementById("text2").style.display = "none";
+
+function showText1() {
+  document.getElementById("text1").style.display = "block";
+  document.getElementById("text2").style.display = "none";
+}
+
+function showText2() {
+  document.getElementById("text1").style.display = "none";
+  document.getElementById("text2").style.display = "block";
+}
